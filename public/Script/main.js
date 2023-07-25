74a68d39873b69d8c5495fa98b0a1c40e12bcf45
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-07-25 13:55:54 +0330

    add change js

--- public/Script/main.js
+++ public/Script/main.js
@@ -4,8 +4,10 @@ const taskInput = document.querySelector(".task-input input"),
   taskBox = document.querySelector(".task-box");
 
 let editId,
-  isEditTask = false,
-  todos = JSON.parse(localStorage.getItem("todo-list"));
+  isEditTask = false
+  let todos = JSON.parse(localStorage.getItem("todo-list")) || [];
+//   const todos = JSON.parse(localStorage.getItem("todo-list")) || [];
+
 
 filters.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -15,60 +17,71 @@ filters.forEach((btn) => {
   });
 });
 
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+if (localStorage.getItem("todos")) {
+  todos.map((task) => {
+    createTask(task);
+  });
+}
 
 taskInput.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const inputValue = mainInput.value;
+  const inputValue = taskInput.value;
 
-  if ((inputValue = "")) {
+  if (inputValue === "") {
     return;
-  }
+  }  
 
   const task = {
     id: new Date().getTime(),
     name: inputValue,
-    isCompleted: false
+    isCompleted: false,
   };
 
-  tasks.push(task)
-  localStorage.setItem('tasks' , JSON.stringify(tasks))
+  todos.push(task);
+  localStorage.setItem("todos", JSON.stringify(todos));
 
-  createTask(task)
+  createTask(task);
 
-  todoForm.reset()
-  mainInput.focus()
-
+  todoForm.reset();
+  mainInput.focus();
 });
 
-function createTask(task){
-    const CompletedTasksArray = tasks.filter((task) =>{
-        task.isCompleted === true 
-    })
+function createTask(task) {
+  const taskEl = document.createElement("li");
+
+  taskEl.setAttribute("id", task.id);
+
+  if (task.isCompleted) {
+    taskEl.classList.add("complete");
+  }
+
+  let liTag = "";
+  if (todos) {
+    todos.forEach((todo, id) => {
+      let completed = todo.status == "completed" ? "checked" : "";
+      if (btn.id == todo.status || btn.id == "all") {
+        liTag += `<li class="task">
+                          <label for="${id}">
+                              <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
+                              <p class="${completed}">${todo.name}</p>
+                          </label>
+                          <div class="settings">
+                              <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+                              <ul class="task-menu">
+                                  <li onclick='editTask(${id}, "${todo.name}")'><i class="uil uil-pen"></i>Edit</li>
+                                  <li onclick='deleteTask(${id}, "${btn.id}")'><i class="uil uil-trash"></i>Delete</li>
+                              </ul>
+                          </div>
+                      </li>`;
+      }
+    });
+  }
 }
 
 
-function showTodo(filter) {
-    let liTag = "";
-    if (todos) {
-      todos.forEach((todo, id) => {
-        let completed = todo.status == "completed" ? "checked" : "";
-        if (filter == todo.status || filter == "all") {
-          liTag += `<li class="task">
-                              <label for="${id}">
-                                  <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
-                                  <p class="${completed}">${todo.name}</p>
-                              </label>
-                              <div class="settings">
-                                  <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
-                                  <ul class="task-menu">
-                                      <li onclick='editTask(${id}, "${todo.name}")'><i class="uil uil-pen"></i>Edit</li>
-                                      <li onclick='deleteTask(${id}, "${filter}")'><i class="uil uil-trash"></i>Delete</li>
-                                  </ul>
-                              </div>
-                          </li>`;
-        }
-      });
-    }
-}
+// function showTodo(filter) {}
+// const CompletedtodosArray = todos.filter((task) => {
+//     task.isCompleted === true;
+//   });
