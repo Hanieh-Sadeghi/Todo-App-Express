524d74113133b0444eea7490fae60da10d6aaf30
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-07-25 14:03:39 +0330

    add js

--- public/Script/main.js
+++ public/Script/main.js
@@ -4,10 +4,9 @@ const taskInput = document.querySelector(".task-input input"),
   taskBox = document.querySelector(".task-box");
 
 let editId,
-  isEditTask = false
-  let todos = JSON.parse(localStorage.getItem("todo-list")) || [];
+  isEditTask = false;
+let todos = JSON.parse(localStorage.getItem("todo-list")) || [];
 //   const todos = JSON.parse(localStorage.getItem("todo-list")) || [];
-
 
 filters.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -16,7 +15,6 @@ filters.forEach((btn) => {
     showTodo(btn.id);
   });
 });
-
 
 if (localStorage.getItem("todos")) {
   todos.map((task) => {
@@ -31,7 +29,7 @@ taskInput.addEventListener("submit", (e) => {
 
   if (inputValue === "") {
     return;
-  }  
+  }
 
   const task = {
     id: new Date().getTime(),
@@ -62,24 +60,43 @@ function createTask(task) {
     todos.forEach((todo, id) => {
       let completed = todo.status == "completed" ? "checked" : "";
       if (btn.id == todo.status || btn.id == "all") {
-        liTag += `<li class="task">
-                          <label for="${id}">
-                              <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
-                              <p class="${completed}">${todo.name}</p>
-                          </label>
-                          <div class="settings">
-                              <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
-                              <ul class="task-menu">
-                                  <li onclick='editTask(${id}, "${todo.name}")'><i class="uil uil-pen"></i>Edit</li>
-                                  <li onclick='deleteTask(${id}, "${btn.id}")'><i class="uil uil-trash"></i>Delete</li>
-                              </ul>
-                          </div>
-                      </li>`;
+        liTag += `
+        <li class="task">
+                <label for="${id}">
+                    <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
+                         <p class="${completed}">${todo.name}</p>
+                </label>
+                 <div class="settings">
+                    <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+                        <ul class="task-menu">
+                            <li onclick='editTask(${id}, "${todo.name}")'><i class="uil uil-pen"></i>Edit</li>
+                            <li onclick='deleteTask(${id}, "${btn.id}")'><i class="uil uil-trash"></i>Delete</li>
+                        </ul>
+                        </div>
+                    </li>`;
       }
     });
   }
+  taskBox.innerHTML = liTag || `<span>You don't have any task here</span>`;
+  let checkTask = taskBox.querySelectorAll(".task");
+  !checkTask.length
+    ? clearAll.classList.remove("active")
+    : clearAll.classList.add("active");
+  taskBox.offsetHeight >= 300
+    ? taskBox.classList.add("overflow")
+    : taskBox.classList.remove("overflow");
 }
+showTodo("all");
 
+function showMenu(selectedTask) {
+  let menuDiv = selectedTask.parentElement.lastElementChild;
+  menuDiv.classList.add("show");
+  document.addEventListener("click", (e) => {
+    if (e.target.tagName != "I" || e.target != selectedTask) {
+      menuDiv.classList.remove("show");
+    }
+  });
+}
 
 // function showTodo(filter) {}
 // const CompletedtodosArray = todos.filter((task) => {
