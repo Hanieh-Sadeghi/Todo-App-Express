fba6c71a4a234ff944ef24d1c04f18de787d5642
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-08-12 23:13:13 +0330

    add js

--- public/Script/view.js
+++ public/Script/view.js
@@ -1,28 +1,34 @@
 const todoForm = document.querySelector("#task-input");
 const input = document.querySelector("input[type='text']");
 
+
 document.getElementById("add-btn").addEventListener("click", function () {
-  const todoText = document.getElementById("todoInput").value;
-  if (todoText !== "") {
-    const listEl = document.querySelector(".task-box");
-    const newTask = document.querySelector(".task-box");
-    newTask.classList.add("task");
-    newTask.innerHTML = `
-        <label>
-        <input type="checkbox" />
-        <p>${todoText}</p>
-    </label>
-    <div class="select">
-        <i class="uil uil-ellipsis-h"></i>
-    </div>
-    <ul class="task-menu">
-        <li><i class="uil uil-pen"></i>Edit</li>
-        <li><i class="uil uil-trash"></i>Delete</li>
-    </ul> `;
-    listEl.appendChild(newTask)
-    document.getElementById('todoInput').value = ''
-  }
-});
+    const todoText = document.getElementById("todoInput").value;
+    if (todoText !== "") {
+      const listEl = document.querySelector(".task-box");
+    
+      const newTask = document.createElement("li");
+      newTask.classList.add("task");
+      newTask.innerHTML = `
+          <label>
+            <input type="checkbox" />
+            <p>${todoText}</p>
+          </label>
+          <div class="select">
+            <i class="uil uil-ellipsis-h"></i>
+          </div>
+          <ul class="task-menu">
+            <li><i class="uil uil-pen"></i>Edit</li>
+            <li><i class="uil uil-trash"></i>Delete</li>
+          </ul> `;
+      
+
+      listEl.appendChild(newTask);
+      
+      document.getElementById('todoInput').value = '';
+    }
+  });
+  
 
 ////////// /////////////////////
 document.addEventListener("DOMContentLoaded", function () {
