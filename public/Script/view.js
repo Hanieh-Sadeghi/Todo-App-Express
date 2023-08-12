86279e80509a2325d6eef2a35a628fd2bba69aa5
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-08-12 20:36:24 +0330

    add js new tasks

--- public/Script/view.js
+++ public/Script/view.js
@@ -1,21 +1,45 @@
-// const todoForm = document.querySelector("#task-input");
-// const input = document.querySelector("#");
+const todoForm = document.querySelector("#task-input");
+const input = document.querySelector("input[type='text']");
 
-// const listEl = document.querySelector("#tasks");
+document.getElementById("add-btn").addEventListener("click", function () {
+  const todoText = document.getElementById("todoInput").value;
+  if (todoText !== "") {
+    const listEl = document.querySelector(".task-box");
+    const newTask = document.querySelector(".task-box");
+    newTask.classList.add("task");
+    newTask.innerHTML = `
+        <label>
+        <input type="checkbox" />
+        <p>${todoText}</p>
+    </label>
+    <div class="select">
+        <i class="uil uil-ellipsis-h"></i>
+    </div>
+    <ul class="task-menu">
+        <li><i class="uil uil-pen"></i>Edit</li>
+        <li><i class="uil uil-trash"></i>Delete</li>
+    </ul> `;
+    listEl.appendChild(newTask)
+    document.getElementById('todoInput').value = ''
+  }
+});
+
+////////// /////////////////////
 document.addEventListener("DOMContentLoaded", function () {
-    const selectIcon = document.querySelector(".select i");
-    const taskMenu = document.querySelector(".task-menu");
-  
-    selectIcon.addEventListener("click", function (event) {
-      event.stopPropagation();
-      taskMenu.classList.toggle("show-menu");
-    });
-  
- 
-    document.addEventListener("click", function (event) {
-      if (!taskMenu.contains(event.target) && !selectIcon.contains(event.target)) {
-        taskMenu.classList.remove("show-menu");
-      }
-    });
+  const selectIcon = document.querySelector(".select i");
+  const taskMenu = document.querySelector(".task-menu");
+
+  selectIcon.addEventListener("click", function (event) {
+    event.stopPropagation();
+    taskMenu.classList.toggle("show-menu");
   });
-  
+
+  document.addEventListener("click", function (event) {
+    if (
+      !taskMenu.contains(event.target) &&
+      !selectIcon.contains(event.target)
+    ) {
+      taskMenu.classList.remove("show-menu");
+    }
+  });
+});
