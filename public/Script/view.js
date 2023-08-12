910ba1eddedc567d32c9471112aa23d093fbc50e
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-08-12 13:01:45 +0330

    add JS new

--- public/Script/view.js
+++ public/Script/view.js
@@ -1,4 +1,21 @@
-const todoForm = document.querySelector("#task-input");
-const input = document.querySelector("#");
+// const todoForm = document.querySelector("#task-input");
+// const input = document.querySelector("#");
 
 // const listEl = document.querySelector("#tasks");
+document.addEventListener("DOMContentLoaded", function () {
+    const selectIcon = document.querySelector(".select i");
+    const taskMenu = document.querySelector(".task-menu");
+  
+    selectIcon.addEventListener("click", function (event) {
+      event.stopPropagation();
+      taskMenu.classList.toggle("show-menu");
+    });
+  
+ 
+    document.addEventListener("click", function (event) {
+      if (!taskMenu.contains(event.target) && !selectIcon.contains(event.target)) {
+        taskMenu.classList.remove("show-menu");
+      }
+    });
+  });
+  
