7cee22f150d1c7c1dd368228ac7f87d92661cad7
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-08-22 19:58:31 +0330

    add theme localStorage

--- public/Script/theme.js
+++ public/Script/theme.js
@@ -1,17 +1,26 @@
-console.log("hanieh");
 
-let  root =  document.querySelector ('html');
-let btn = document.getElementById('themeBtn');
+let root = document.querySelector("html");
+let btn = document.getElementById("themeBtn");
 
-console.log(btn)
-btn.addEventListener('click', function () {
+const savedTheme = localStorage.getItem("selectedTheme");
 
-console.log(btn)
-    if ( root .getAttribute ('data-theme') === 'Jungle') {
-        root .setAttribute ('data-theme' , 'canad');
-    } else if ( root .getAttribute('data-theme') === 'canad'){
-        root.setAttribute('data-theme' , 'city');
-    }else if ( root .getAttribute('data-theme') === 'city'){
-        root.setAttribute('data-theme' , 'Jungle');
-    }
-})
+if (savedTheme) {
+  root.setAttribute("data-theme", savedTheme);
+}
+
+btn.addEventListener("click", function () {
+
+  if (root.getAttribute("data-theme") === "theme-container") {
+    root.setAttribute("data-theme", "canada");
+
+  } else if (root.getAttribute("data-theme") === "canada") {
+    root.setAttribute("data-theme", "Snow");
+  } else if (root.getAttribute("data-theme") === "Snow") {
+    root.setAttribute("data-theme", "jungl");
+  } else if (root.getAttribute("data-theme") === "jungl") {
+    root.setAttribute("data-theme", "theme-container");
+  }
+
+  localStorage.setItem("selectedTheme", root.getAttribute("data-theme"));
+
+});
