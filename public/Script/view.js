8ed278ca4d60ddca7cfcb827f6705cc4d77dc5f1
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-09-04 00:33:15 +0330

    add new change

--- public/Script/view.js
+++ public/Script/view.js
@@ -12,45 +12,38 @@ let identifier = 0;
 filters = document.querySelectorAll(".header span");
 
 let filterKind = "all";
-// const tasks = [];
-let tasks = {};
+
 let TaskId;
 
-// Load tasks from local storage
-function loadTasks() {
-  const memory = localStorage.getItem(taskKey);
-  const tasksData = JSON.parse(memory) || {};
-  listEl.innerHTML = "";
-  const todoList = Object.values(tasksData);
-
-  // const filteredTasks = todoList.filter((task) => {
-  //   if (filterKind === "active") return !task.completed;
-  //   if (filterKind === "completed") return task.completed;
-  //   return true;
-  // });
-
-  todoList.forEach((task) => {
-    createTaskElement(task.text, task.id, task.edit);
-  });
-}
-
-filters.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const filterType = btn.id.toLowerCase();
-    document.querySelector("span.active").classList.remove("active");
-    btn.classList.add("active");
-    filterTasks(filterType);
+all.addEventListener("click", () => {
+  const todos = filterTasks("all");
+  todos.forEach((todo) => {
+    createTaskElement(todo.text, todo.id, todo.edit, todo.completed);
   });
 });
+active.addEventListener("click", () => filterTasks("active"));
+completed.addEventListener("click", () => filterTasks("completed"));
+
+// Load tasks from local storage
+// function loadTasks() {
+//   const memory = localStorage.getItem(taskKey);
+//   const tasksData = JSON.parse(memory) || [];
+//   listEl.innerHTML = "";
+// const todoList = Object.values(tasksData);
+
+//   tasksData.forEach((task) => {
+//     createTaskElement(task.text, task.id, task.completed);
+//   });
+// }
 
 // Create a task element
-function createTaskElement(taskText, id, edit) {
+function createTaskElement(taskText, id, edit ,completed) {
   const newTask = document.createElement("li");
   newTask.classList.add("task");
   newTask.id = `task-${id}`;
   newTask.innerHTML = `
     <label>
-      <input type="checkbox"/>
+    <input type="checkbox" checked=${completed}/> 
     </label>
     <p class="text-center" contenteditable="${edit}">${taskText}</p>
     <div class="select">
@@ -69,9 +62,9 @@ function createTaskElement(taskText, id, edit) {
 function addNewTask() {
   if (input.value.trim() !== "") {
     createTaskElement(input.value.trim());
-    saveTasksToLocalStorage(todo);
+    // saveTasksToLocalStorage(todo);
     saveTasksToLocalStorage(input.value.trim());
-    loadTasks();
+   
   }
   input.value = "";
 }
@@ -79,20 +72,21 @@ function addNewTask() {
 // Save tasks to local storage
 function saveTasksToLocalStorage(input) {
   const memory = localStorage.getItem(taskKey);
-  tasks = JSON.parse(memory) || {};
+  tasks = JSON.parse(memory) || [];
   const keys = Object.keys(tasks);
 
   if (keys.length !== 0 || identifier !== 0) {
     identifier = +keys[keys.length - 1] + 1;
   }
-  tasks[identifier] = {
+  tasks.push({
     text: input,
     id: identifier,
     edit: false,
     completed: false,
-  };
+  });
   ++identifier;
   localStorage.setItem(taskKey, JSON.stringify(tasks));
+  return;
 }
 
 // Delete all tasks from LocalStorage and the page
@@ -105,25 +99,35 @@ function deleteAllTasks() {
 function handleCheckboxChange(event) {
   const checkbox = event.target;
   if (checkbox.type === "checkbox") {
-    const taskId = checkbox.closest("li").id.replace("task-", "");
     const textElement = checkbox.parentElement.nextElementSibling;
-    const task = tasks[taskId];
-
+    const elemntId = checkbox.parentElement.parentElement.id.replace(
+      "task-",
+      ""
+    );
+    const tasks = JSON.parse(localStorage.getItem(taskKey)) || [];
+    const newTask = tasks.filter((task) => {
+      return task.id != elemntId;
+    });
+    console.log(newTask);
     if (checkbox.checked) {
       textElement.style.textDecoration = "line-through";
-      task.completed = true;
+
+      const found = tasks.find((task) => {
+        return task.id == elemntId;
+      });
+      found.completed = true;
+      newTask.push(found);
     } else {
       textElement.style.textDecoration = "none";
-      task.completed = false;
     }
-    localStorage.setItem(taskKey, JSON.stringify(tasks));
+    localStorage.setItem(taskKey, JSON.stringify(newTask));
   }
 }
 
 listEl.addEventListener("change", handleCheckboxChange);
 
 // Event listener for loading tasks
-window.addEventListener("load", loadTasks);
+// window.addEventListener("load", loadTasks);
 
 // Event listener for adding a new task
 addTasks.addEventListener("click", addNewTask);
@@ -161,18 +165,16 @@ document.addEventListener("DOMContentLoaded", setupTaskMenus);
 
 // Delete Task arry
 function deleteTask(deleteId) {
-  const tasks = JSON.parse(localStorage.getItem(taskKey)) || {};
+  const tasks = JSON.parse(localStorage.getItem(taskKey)) || [];
   const filteredTasks = tasks.filter((todo) => {
     return todo.id !== +deleteId;
   });
   localStorage.setItem("tasks", JSON.stringify(filteredTasks));
-  loadTasks();
 }
 
 // Edit Task
 function editTask(editId) {
-  // console.log(editId, taskText);
-  const tasks = JSON.parse(localStorage.getItem(taskKey)) || {};
+  const tasks = JSON.parse(localStorage.getItem(taskKey)) || [];
   const selectedText = document.querySelector(`li#task-${editId}`).children[1]
     .textContent;
   tasks.forEach((todo) => {
@@ -183,22 +185,32 @@ function editTask(editId) {
   });
 
   localStorage.setItem("tasks", JSON.stringify(tasks));
-  loadTasks();
 }
 
 // filterTasks completed active
-// function filterTasks() {
-//   const memory = localStorage.getItem(taskKey);
-//   let tasks = JSON.parse(memory) || {};
-//   tasks = Object.values(tasks);
-//   const completed = tasks.filter((item) => {
-//     return item.completed == true;
-//   });
+function filterTasks(mosi) {
+  const memory = localStorage.getItem(taskKey);
+  let tasks = JSON.parse(memory) || [];
+  all.classList.remove("active");
+  active.classList.remove("active");
+  completed.classList.remove("active");
 
-//   console.log(completed);
-//   listEl.innerHTML = "";
-//   completed.forEach((task) => {
-//     createTaskElement(task.text, task.id, task.edit);
+  let todos = tasks;
 
-//   });
-// }
+  if (mosi === "active") {
+    active.classList.add("active");
+    todos = tasks.filter((task) => {
+      return task.completed === false;
+    });
+    return todos;
+  } else if (mosi === "completed") {
+    completed.classList.add("active");
+    todos = tasks.filter((task) => {
+      return task.completed === true;
+    });
+    return todos;
+  } else {
+    all.classList.add("active");
+    return tasks;
+  }
+}
