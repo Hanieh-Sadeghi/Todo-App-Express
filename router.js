ca3b829e41940e644b3faa953a651a7b2d7efacd
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-09-13 17:35:36 +0330

    delete api

--- router.js
+++ router.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const controller = require("./controller");
 
-router.get("/" , controller.getJson);
-router.post("/" , controller.postJson);
-router.put("/" , controller.putJson);
-
+router.get("/", controller.getJson);
+router.post("/", controller.postJson);
+router.put("/", controller.putJson);
+router.delete("/ :id", controller.deleteJson);
 
 module.exports = router;
