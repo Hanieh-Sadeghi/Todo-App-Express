11eabb47fb1873bc8abfcf92b92672ac9d50fec8
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-09-13 16:44:53 +0330

    add put && post

--- router.js
+++ router.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const controller = require("./controller");
 
 router.get("/" , controller.getJson);
+router.post("/" , controller.postJson);
+router.put("/" , controller.putJson);
+
 
 module.exports = router;
