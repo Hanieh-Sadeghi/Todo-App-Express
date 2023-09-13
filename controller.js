08a83c96a6825f8d658e644854f1a02ac9d21ed3
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-09-13 16:44:05 +0330

    add update

--- controller.js
+++ controller.js
@@ -1,9 +1,34 @@
 const db = require("./model");
 
 function getJson(req, res) {
-    res.json(db)
+  res.json(db);
+}
+
+function postJson(req, res) {
+  db.forEach((data) => {
+    if (data.id == req.body.id) {
+      data.name = req.body.name;
+      data.status = req.body.status;
+    }
+  });
+
+  
+  res.json({
+    response: "data update",
+    db: db,
+  });
+}
+
+function putJson(req, res) {
+  db.push(req.body);
+  res.json({
+    response: "data save",
+    db: db,
+  });
 }
 
 module.exports = {
   getJson,
+  postJson,
+  putJson,
 };
