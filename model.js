20e78c59cb255a9ff4644b7f245ebe296184cf03
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-09-13 19:59:02 +0330

    add usrname

--- model.js
+++ model.js
@@ -1,10 +1,17 @@
 const db = [
   {
-    id: 1,
-    name: "hanieh sadeghi",
-    status: true,
+    name: "test",
+    username: "test",
+    password: "test",
+    email: "test@test.com",
+    token: "test",
+    data: [
+      {
+        id: 1,
+        status: "doing",
+      },
+    ],
   },
 ];
 
-
-module.exports = db; 
+module.exports = db;
