bed82288fcbdfda9af1c9522bd06e06d34a845be
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-09-14 23:58:13 +0330

    add

--- client/js/controller.js
+++ client/js/controller.js
@@ -4,15 +4,21 @@ const download = document.getElementById("download");
 const upload = document.getElementById("upload");
 
 const url = "http://localhost:3000/v1/api";
-console.log('download')
 
-fetch(url) 
+download.addEventListener('click' , () =>{
+  fetch(url) 
  .then((response) => {
-  console.log(response)
-  // console.log(download)
+  // console.log(response)
+  console.log(download)
 
   //  return response.json();
 });
+
+
+})
+
+
+
 
 // .then((data) => {
 //   let authors = data;
