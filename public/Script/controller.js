cbbfdd3c488e6494763a5b0024ae94ff0a5e00b6
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-09-14 17:14:45 +0330

    add

---
+++ public/Script/controller.js
@@ -0,0 +1,15 @@
+// import _view from "./view.js";
+
+const download = document.getElementById("download");
+const upload = document.getElementById("upload");
+
+const url = "http://localhost:3000/v1/api";
+
+fetch(url).then((response) => {
+  return response.json();
+});
+
+// .then((data) => {
+//   let authors = data;
+
+// })
