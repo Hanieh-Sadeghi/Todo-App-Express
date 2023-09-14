48cb04252fe6fa1aed05ebf0c264bc57d23a87c0
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-09-14 23:36:51 +0330

    add change

--- client/js/controller.js
+++ client/js/controller.js
@@ -4,10 +4,13 @@ const download = document.getElementById("download");
 const upload = document.getElementById("upload");
 
 const url = "http://localhost:3000/v1/api";
+console.log('download')
 
 fetch(url) 
  .then((response) => {
-  console(response)
+  console.log(response)
+  // console.log(download)
+
   //  return response.json();
 });
 
