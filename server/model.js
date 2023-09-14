226fe99622fd85e1b10b91e5c1834f6cdcb8e124
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-09-14 19:59:03 +0330

    add server

--- server/model.js
+++ server/model.js
@@ -1,17 +1,54 @@
-const db = [
-  {
-    name: "test",
-    username: "test",
-    password: "test",
-    email: "test@test.com",
-    token: "test",
-    data: [
-      {
-        id: 1,
-        status: "doing",
-      },
-    ],
-  },
-];
+// const db = [
+//   {
+//     name: "test",
+//     username: "test",
+//     password: "test",
+//     email: "test@test.com",
+//     token: "test",
+//     data: [
+//       {
+//         id: 1,
+//         status: "doing",
+//       },
+//     ],
+//   },
+// ];
 
-module.exports = db;
+// module.exports = db;
+
+const fs = require('fs');
+const path = require('path');
+
+const todosPath = path.join(__dirname, '/db.json')
+
+function read_data(callBack) {
+    fs.readFile(todosPath, 'utf-8', (error, data) => {
+        if (error) return callBack('', error);
+        json_data = JSON.parse(data);
+        callBack(json_data, '');
+    })
+}
+
+function write_data(data, callBack) {
+    fs.writeFile(todosPath, JSON.stringify(data), (res, err) => {
+        if (callBack) callBack(res, err);
+    });
+}
+
+
+function checkToken(token, callBack) {
+    read_data((data, error) => {
+        var status = false;
+        var user;
+        data.forEach(element => {
+            if (element.token == token) return status = true, user = element;
+        })
+        callBack(status, user, data);
+    });
+}
+
+module.exports = {
+    read_data,
+    write_data,
+    checkToken,
+};
