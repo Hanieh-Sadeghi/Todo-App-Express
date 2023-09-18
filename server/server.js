c81e174bf46c527303c636d602bfd00cb35408e0
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-09-19 00:58:57 +0330

    add path

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = require("./router");
+const path = require("path");
+
 const app = express();
 const PORT = 3000;
 
-app.use(express.json())
+app.use(express.json());
 app.use("/v1/api", router);
+app.use(express.static(path.join(__dirname ,'../client')))
+
 
 app.listen(3000, () => {
   console.log(`listening pn port ${PORT}`);
