96fa53c22b8a54cb77fe6105761c08d55aabaa13
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-09-18 15:11:38 +0330

    add server

---
+++ server/router.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const controller = require('./controller')
+
+
+router.get("/", controller.getJson ); 
+router.post("/", controller.postJson ); 
+router.put("/", controller.putJson ); 
+router.delete("/:id", controller.deleteJson ); 
+
+
+module.exports = router;
