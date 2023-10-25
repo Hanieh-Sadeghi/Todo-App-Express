e89bab3692d9491aa22a714ac02b0cc59e50cb79
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-10-26 01:50:08 +0330

    add app login

--- server/server.js
+++ server/server.js
@@ -1,19 +1,52 @@
 const express = require("express");
 const router = require("./router");
 const path = require("path");
-const bodyParser= require('body-parser')
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use("/v1/api", router);
-app.use(express.static(path.join(__dirname ,'../client')))
-app.use(bodyParser.urlencoded({extended : false }))
+app.use(express.static(path.join(__dirname, "../client")));
+app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(bodyParser.json)
 
+const users = [];
+const bcrypt = require("bcrypt");
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.post("/users", async (req, res) => {
+  try {
+    // const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password.salt);
+    const user = { name: req.body.name, password: req.body.password };
+    users.push(user);
+    res.status(201).send();
+  } catch {
+    res.status(500).send();
+  }
+});
+
+app.post("/users/login", async (req, res) => {
+  const user = users.find((user) => (user.name = req.body.name));
+  if (user == null) {
+    return res.status(400).send("Cannot find user");
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send("Success");
+    } else {
+      res.send("Not Allowed");
+    }
+  } catch {
+    res.status(500).send();
+  }
+});
 
 app.listen(3000, () => {
   console.log(`listening pn port ${PORT}`);
 });
-
